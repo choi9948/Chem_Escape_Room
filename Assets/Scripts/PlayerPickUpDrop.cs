bcbf13857c1d563f62ea9aeacef6681191a61e3e
richardgli <ric.li6374@gmail.com>
2025-06-01 13:47:16 -0400

    Player Pick Up and Drop #2

---
+++ Assets/Scripts/PlayerPickUpDrop.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerPickUpDrop : MonoBehaviour
+{
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
+    [SerializeField] private LayerMask pickUpLayerMask;
+
+    private ObjectGrabbable objectGrabbable;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (objectGrabbable == null)
+            {
+                // Not carrying an object, try to grab
+                float pickUpDistance = 3f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance))
+                {
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                    }
+                }
+            }
+            else
+            {
+                // Currently carrying something, drop
+                objectGrabbable.Drop();
+                objectGrabbable = null;
+            } 
+        }
+    }
+}
