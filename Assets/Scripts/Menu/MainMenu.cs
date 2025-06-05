896c5d3b1a9e0ac012fcb41b12c97203eecff61b
richardgli <ric.li6374@gmail.com>
2025-06-05 01:59:54 -0400

    Pause and main menu update

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,7 +5,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int prevRoomIndex = PlayerPrefs.GetInt("Previous Room", 1);
+        SceneManager.LoadScene(prevRoomIndex);
     }
 
     public void QuitGame()
--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -1,10 +1,50 @@
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        Cursor.visible = true;
+    }
+
     public void ReturnToMenu()
     {
+        Time.timeScale = 1f;
+        GamePaused = false;
+        Cursor.visible = true;
+        PlayerPrefs.SetInt("Previous Room", SceneManager.GetActiveScene().buildIndex);
+        PlayerPrefs.Save();
         SceneManager.LoadScene(0);
     }
 
